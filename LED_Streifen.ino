5df5b4e1c5d271b4d0cf4e8810cd2f91ee3e1e4a
Marco Pfeiffer <git@marco.zone>
2019-11-09 21:43:50 +0100

    add timings and optimize a bit

--- LED_Streifen.ino
+++ LED_Streifen.ino
@@ -5,7 +5,6 @@
 #define LED_PIN         6
 #define LED_COUNT       100
 
-#define EFFECT_COUNT    2
 #define SWITCH_DURATION (1024 * 4)
 #define FADE_DURATION   (1024)
 #define STAY_DURATION   (SWITCH_DURATION - FADE_DURATION)
@@ -16,9 +15,12 @@ Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_BRG + NEO_KHZ800);
 void setup() {
   strip.begin();
   strip.setBrightness(0x0f);
+  Serial.begin(9600);
 }
 
 void loop() {
+  auto startTime = micros();
+  
   auto time = millis();
   auto effect = time / SWITCH_DURATION;
   auto fade = time % SWITCH_DURATION;
@@ -29,27 +31,30 @@ void loop() {
   }
 
   auto fadePosition = strip.numPixels() * fade / FADE_DURATION;
-  
-  #if FADE_DIRECTION == 0
-  renderEffect(strip, effect % EFFECT_COUNT, fadePosition, strip.numPixels());
-  if (fadePosition > 0) {
-    renderEffect(strip, (effect + 1) % EFFECT_COUNT, 0, fadePosition);
-  }
-  #endif
 
-  #if FADE_DIRECTION == 1
-  renderEffect(strip, effect % EFFECT_COUNT, 0, strip.numPixels() - fadePosition);
+#if FADE_DIRECTION == 0
+  renderEffect(strip, effect, fadePosition, strip.numPixels());
   if (fadePosition > 0) {
-    renderEffect(strip, (effect + 1) % EFFECT_COUNT, strip.numPixels() - fadePosition, strip.numPixels());
+    renderEffect(strip, effect + 1, 0, fadePosition);
   }
-  #endif
-  
-  
+#endif
+
+#if FADE_DIRECTION == 1
+  renderEffect(strip, effect, 0, strip.numPixels() - fadePosition);
+  if (fadePosition > 0) {
+    renderEffect(strip, effect + 1, strip.numPixels() - fadePosition, strip.numPixels());
+  }
+#endif
+
+  auto endTime = micros();
+  Serial.print(endTime - startTime);
   strip.show();
+  Serial.print("\t");
+  Serial.println(micros() - endTime);
 }
 
 void renderEffect(Adafruit_NeoPixel &strip, const int &effect, const uint16_t &first, const uint16_t &last) {
-  switch (effect) {
+  switch (effect % 2) {
     case 0:
       RgbEffect::run(strip, first, last);
       break;
--- RgbEffect.h
+++ RgbEffect.h
@@ -5,26 +5,26 @@
 #include "Color.h"
 
 namespace RgbEffect {
-  using namespace Color;
+using namespace Color;
 
-  const color colors[] = {0xff0000, 0x00ff00, 0x0000ff};
-  const uint8_t COLOR_WIDTH = 20;
-  
-  const uint8_t COLOR_COUNT = sizeof(colors) / sizeof(color);
-  const uint8_t PIXEL_TIME_SHIFT = MAX_PROGRESS / COLOR_WIDTH;
-  
-  void run (Adafruit_NeoPixel &strip, const uint16_t &first, const uint16_t last) {
-    auto time = millis() / 4;
-    
-    for (auto i = first; i <= last; ++i) {
-      auto adjustedTime = time + i * PIXEL_TIME_SHIFT;
-      auto progress = adjustedTime % MAX_PROGRESS;
-      auto index = adjustedTime / MAX_PROGRESS;
-      auto color1 = colors[(index) % COLOR_COUNT];
-      auto color2 = colors[(index + 1) % COLOR_COUNT];
-      strip.setPixelColor(i, transition(color1, color2, progress));
-    }
+const color colors[] = {0xff0000, 0x00ff00, 0x0000ff};
+const uint8_t COLOR_WIDTH = 20;
+
+const uint8_t COLOR_COUNT = sizeof(colors) / sizeof(color);
+const uint8_t PIXEL_TIME_SHIFT = MAX_PROGRESS / COLOR_WIDTH;
+
+void run (Adafruit_NeoPixel &strip, const uint16_t &first, const uint16_t last) {
+  uint16_t time = millis() / 4;
+
+  for (auto i = first; i < last; ++i) {
+    auto adjustedTime = time + i * PIXEL_TIME_SHIFT;
+    auto progress = adjustedTime % MAX_PROGRESS;
+    auto index = adjustedTime / MAX_PROGRESS;
+    auto color1 = colors[(index) % COLOR_COUNT];
+    auto color2 = colors[(index + 1) % COLOR_COUNT];
+    strip.setPixelColor(i, transition(color1, color2, progress));
   }
+}
 }
 
 #endif
